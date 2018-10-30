8336ae44104d4ec232e01b6e76873a699fa151ba
87717663@qq.com <Yuxl>
2018-10-30 11:42:36 +0800

    新增Socket基础类库

---
+++ Sockets/abstractSockets.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+
+namespace Sockets
+{
+    /// <summary>
+    /// 定义一个Scoket初始化的抽象类,子类继承需要重写
+    /// </summary>
+    public abstract class abstractSockets
+    {
+        /// <summary>
+        /// 初始化Socket方法
+        /// </summary>
+        /// <param name="ipAddress">ip终结点</param>
+        /// <param name="port">端口</param>
+        public abstract void InitSocket(IPAddress ipAddress, int port);
+        public abstract void InitSocket(string ipAddress, int port);
+
+        /// <summary>
+        /// Socket启动方法
+        /// </summary>
+        public abstract void Start();
+
+        /// <summary>
+        /// Sockdet停止方法
+        /// </summary>
+        public abstract void Stop();
+    }
+}
